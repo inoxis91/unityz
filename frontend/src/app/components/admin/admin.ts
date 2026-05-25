02afaa9809c474681d461fd820b0dd6fc0d5275a
Florian Delpech <florian_delpech@carrefour.com>
2026-05-25 20:20:38 +0200

    Cleanup: remove events tab from admin menu and clean up associated code

--- frontend/src/app/components/admin/admin.ts
+++ frontend/src/app/components/admin/admin.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { CalendarService, CalendarEvent } from '../../services/calendar';
 import { AdminRostersComponent } from './admin-rosters/admin-rosters';
 import { AdminFeesComponent } from './admin-fees/admin-fees';
 
@@ -13,52 +12,9 @@ import { AdminFeesComponent } from './admin-fees/admin-fees';
   styleUrl: './admin.css'
 })
 export class AdminComponent implements OnInit {
-  activeTab = signal<'events' | 'rosters' | 'fees'>('events');
-  events = signal<CalendarEvent[]>([]);
-  
-  // New Event Form
-  newEvent: CalendarEvent = {
-    title: '',
-    description: '',
-    start_time: '20:30',
-    end_time: '22:30',
-    type: 'raid'
-  };
+  activeTab = signal<'rosters' | 'fees'>('rosters');
 
-  constructor(private calendarService: CalendarService) {}
+  constructor() {}
 
-  ngOnInit() {
-    this.loadEvents();
-  }
-
-  loadEvents() {
-    this.calendarService.getEvents().subscribe(events => {
-      this.events.set(events);
-    });
-  }
-
-  onSubmit() {
-    this.calendarService.createEvent(this.newEvent).subscribe(() => {
-      this.loadEvents();
-      this.resetForm();
-    });
-  }
-
-  deleteEvent(id: string) {
-    if (confirm('Êtes-vous sûr de vouloir supprimer cet événement ?')) {
-      this.calendarService.deleteEvent(id).subscribe(() => {
-        this.loadEvents();
-      });
-    }
-  }
-
-  resetForm() {
-    this.newEvent = {
-      title: '',
-      description: '',
-      start_time: '20:30',
-      end_time: '22:30',
-      type: 'raid'
-    };
-  }
+  ngOnInit() {}
 }
